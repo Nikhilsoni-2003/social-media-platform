1aa16eaf755c5769f8593c5d6786a91ec5743d67
Tonmoy Mukherjee <tonmoymukherjee31@gmail.com>
2025-09-08 10:08:04 +0530

    slash isssue

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:["http://localhost:5173", "https://main.d1o7mnrhj60m.amplifyapp.com/"],
+    origin:["http://localhost:5173", "https://main.d1o7mnrhj60m.amplifyapp.com"],
     credentials:true
 }))
 app.use(express.json())
